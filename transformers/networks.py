2ba79334a9092953442c3f01e067cfa0c07051ec
maksym-petrenko <maksym.petrenko.a@gmail.com>
2024-11-08 02:36:16 +0000

    add multihead attention

--- transformers/networks.py
+++ transformers/networks.py
@@ -5,19 +5,20 @@ import numpy as np
 
 class Attention(nn.Module):
 
-    def __init__(self, input_dim: int, embeddings_size: int):
+    def __init__(self, input_dim: int, embeddings_size: int, masked: bool = False):
 
         super().__init__()
         
         self.input_dim = input_dim
         self.embeddings_size = embeddings_size
+        self.masked = masked
 
         self.q = nn.Linear(embeddings_size, embeddings_size)
         self.k = nn.Linear(embeddings_size, embeddings_size)
         self.v = nn.Linear(embeddings_size, embeddings_size)
 
 
-    def forward(self, x, masked: bool = False):
+    def forward(self, x):
         
         Q = self.q(x)
         K = self.k(x)
@@ -28,7 +29,7 @@ class Attention(nn.Module):
         scores = torch.matmul(Q, K)
         scores = scores / torch.sqrt(self.embeddings_size)
         
-        if masked:
+        if self.masked:
             mask = torch.tril(self.input_dim, self.input_dim).bool().to(x.device)
             scores = masked_fill(mask, float("-inf"))
 
@@ -37,4 +38,33 @@ class Attention(nn.Module):
         x = torch.matmul(scores, V)
         return x
 
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, num_heads: int, input_dim: int, embeddings_size: int, masked: bool = False):
+
+        super().__init__()
+        
+        self.num_heads = num_heads  
+        self.input_dim = input_dim
+        self.embeddings_size = embeddings_size
+        
+        if embeddings_size % heads:
+            raise Ecxeption("`embeddings_size` must be divisible by `heads`!")
+
+        self.local_embed_size = embeddings_size / num_heads
+        
+        self.heads = nn.ModuleList([Attention(input_dim, self.local_embed_size, masked) for _ in range(num_heads)])
+        self.WO = nn.Linear(embeddings_size, embeddings_size)
+
+    def forward(self, x):
+
+        head_chunks = torch.split(x, self.local_embed_size, dim=-1)
+        head_chunks = [head(chunk) for head, chunk in zip(self.num_heads, head_chunks)]
+        
+        concatenated = torch.cat(head_chunks, dim=-1)
+
+        x = self.WO(concatenated)
+
+        return x
 
