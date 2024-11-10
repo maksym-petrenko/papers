0514fd0b3729059cacda81594e7044f75314900b
maksym-petrenko <maksym.petrenko.a@gmail.com>
2024-11-10 10:54:12 +0000

    add LayerNorm, minor fixes, start development of Encoder

--- transformers/networks.py
+++ transformers/networks.py
@@ -5,17 +5,17 @@ import numpy as np
 
 class Attention(nn.Module):
 
-    def __init__(self, input_dim: int, embeddings_size: int, masked: bool = False):
+    def __init__(self, input_dim: int, d_model: int, masked: bool = False):
 
         super().__init__()
         
         self.input_dim = input_dim
-        self.embeddings_size = embeddings_size
+        self.d_model = d_model
         self.masked = masked
 
-        self.q = nn.Linear(embeddings_size, embeddings_size)
-        self.k = nn.Linear(embeddings_size, embeddings_size)
-        self.v = nn.Linear(embeddings_size, embeddings_size)
+        self.q = nn.Linear(d_model, d_model)
+        self.k = nn.Linear(d_model, d_model)
+        self.v = nn.Linear(d_model, d_model)
 
 
     def forward(self, x):
@@ -27,7 +27,7 @@ class Attention(nn.Module):
         K = torch.transpose(K, -2, -1)
 
         scores = torch.matmul(Q, K)
-        scores = scores / torch.sqrt(self.embeddings_size)
+        scores = scores / torch.sqrt(self.d_model)
         
         if self.masked:
             mask = torch.tril(self.input_dim, self.input_dim).bool().to(x.device)
@@ -41,21 +41,26 @@ class Attention(nn.Module):
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, num_heads: int, input_dim: int, embeddings_size: int, masked: bool = False):
+    def __init__(self, 
+            num_heads: int, 
+            input_dim: int, 
+            d_model: int, 
+            masked: bool = False
+        ):
 
         super().__init__()
         
         self.num_heads = num_heads  
         self.input_dim = input_dim
-        self.embeddings_size = embeddings_size
+        self.d_model = d_model
         
-        if embeddings_size % heads:
-            raise Ecxeption("`embeddings_size` must be divisible by `heads`!")
+        if d_model % heads:
+            raise Ecxeption("`d_model` must be divisible by `heads`!")
 
-        self.local_embed_size = embeddings_size / num_heads
+        self.local_embed_size = d_model / num_heads
         
         self.heads = nn.ModuleList([Attention(input_dim, self.local_embed_size, masked) for _ in range(num_heads)])
-        self.WO = nn.Linear(embeddings_size, embeddings_size)
+        self.WO = nn.Linear(d_model, d_model)
 
     def forward(self, x):
 
@@ -67,4 +72,53 @@ class MultiHeadAttention(nn.Module):
         x = self.WO(concatenated)
 
         return x
+
+
+class LayerNorm(nn.Module):
+
+    def __init__(self, d_model: int):
+
+        super.__init__()
+        
+        self.g = nn.Parameter(torch.ones(d_model))
+        self.b = nn.Parameter(torch.zeros(d_model))
+
+    def forward(self, x):
+
+        mean = torch.mean(x, -1, keepdim=True)
+        var = torch.var(x, -1, keepdim=True)
+        
+        epsilon = 1e-12
+
+        x = (x - mean) * self.g / torch.sqrt(var + epsilon) + self.b 
+        
+        return x
+
+
+class TransformersEncoder(nn.Module):
+
+    def __init__(self, 
+            num_heads: int, 
+            input_dim: int, 
+            d_model: int, 
+        ):
+
+        super().__init__()
+        
+        self.mh_attention = MultiHeadAttention(
+            num_heads=num_heads,
+            input_dim=input_dim,
+            d_model=d_model,
+            masked=False
+        )
+
+        self.ffn = nn.Linear(d_model, d_model)
+
+    def forward(self, x):
+        
+        y = self.mh_attention(x)
+        
+        x = x + y
+
+        ## TODO
 
