8debd8a1dd4c15563aa5d3a3e1260e1e6a821aec
maksym-petrenko <maksym.petrenko.a@gmail.com>
2024-11-22 19:17:18 +0000

    add validation

--- mnist.py
+++ mnist.py
@@ -4,7 +4,7 @@ from kan import ReLUKAN
 import torch.optim as optim
 from torchvision.datasets import MNIST
 from torchvision.transforms import Compose, ToTensor
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 
 class ReLUKAN_MNIST(nn.Module):
@@ -18,22 +18,27 @@ class ReLUKAN_MNIST(nn.Module):
 
         x = x.reshape(x.shape[0], -1)
         x = self.kan(x)
-        x = nn.Softmax()(x)
+        x = nn.Softmax(dim=-1)(x)
         
         return x
 
 model = ReLUKAN_MNIST()
 model = model.to('cuda')
-print(next(model.parameters()).device)
-
 
 transform = Compose([ToTensor()])
-train_dataset = MNIST(root='./data', train=True, download=True, transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+dataset = MNIST(root='./data', train=True, download=True, transform=transform)
+
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+train_subset, val_subset = random_split(dataset, [train_size, val_size])
+
+train_loader = DataLoader(train_subset, batch_size=256, shuffle=True)
+val_loader = DataLoader(val_subset, batch_size=1024, shuffle=True)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-5)
 
+verbose = True
 num_epochs = 1000
 
 for epoch in range(num_epochs):
@@ -52,16 +57,19 @@ for epoch in range(num_epochs):
         
         running_loss += loss.item()
     
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss / len(train_loader)}')
-
-model.eval()
-correct = 0
-total = 0
-with torch.no_grad():
-    for inputs, labels in train_loader:
-        outputs = model(inputs)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    if verbose:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss / len(train_loader)}')
     
-    print(f'Accuracy on the training set: {100 * correct / total}%')
+    if not (epoch + 1) % 10:
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to('cuda'), labels.to('cuda')
+                outputs = model(inputs)
+                predicted = torch.argmax(outputs.data, -1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+            
+            print(f'Accuracy on the validation set: {100 * correct / total}%')
