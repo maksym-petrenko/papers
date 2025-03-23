09498be62e4f8b107d71d4ea423f898e446d75e3
maksym-petrenko <maksym.petrenko.a@gmail.com>
2025-03-23 02:48:49 +0000

    set the skeleton of new embeddings

--- transformers/embeddings.py
+++ transformers/embeddings.py
@@ -1,28 +1,28 @@
 import torch
-import torch.nn as nn
-import numpy as np
-from typing import Dict, Optional
+from torch import nn 
 
-def load_glove_embeddings(path: str = 'glove.6B.300d.txt') -> tuple[Dict[str, int], torch.Tensor]:
-    
-    word2idx = {}
-    embeddings_list = []
-    
-    word2idx['<pad>'] = 0
-    embeddings_list.append(torch.zeros(300))
-    
-    word2idx['<unk>'] = 1
-    embeddings_list.append(torch.randn(300))
-    
-    with open(path, 'r', encoding='utf-8') as f:
-        for i, line in enumerate(f, 2):
-            values = line.split()
-            word = values[0]
-            vector = torch.FloatTensor([float(val) for val in values[1:]])
-            
-            word2idx[word] = i
-            embeddings_list.append(vector)
-            
-    embeddings = torch.stack(embeddings_list)
-    return word2idx, embeddings
 
+class Embeddings(nn.Module):
+
+    def __init__(
+            self,
+            d_model: int,         # unchangable
+            vocab_size: int       # can be changed with an internal funcion
+        ):
+
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+
+
+    def forward(self, x):
+
+        pass
+    
+    def extend_vocabulary(self, new_tokens):
+
+        pass
+
+    def reduce_vocabulary(self, del_tokens):
+
+        pass
