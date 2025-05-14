841c15505ea1ef8b7e39efa0c29aa80813cb2d83
maksym-petrenko <maksym.petrenko.a@gmail.com>
2025-05-14 22:06:41 +0000

    minor fixes

--- transformers/train.py
+++ transformers/train.py
@@ -24,27 +24,26 @@ for epoch in tqdm(range(1, epochs + 1)):
         optimizer.zero_grad()
 
         src = str(line["en"])
-        output_tokens = model.embeddings.tokenize(str(line["fr"]))
+        output_tokens = [2] + model.embeddings.tokenize(str(line["fr"]))
 
         total_loss = 0
 
         for i in range(1, len(output_tokens)):
             tokens = output_tokens[:i]
             text = "".join([model.embeddings.id_to_token[token] for token in tokens])
-            print(text)
 
             probs = model(src, text, train=True)
 
-            expected_probs = torch.zeros(model.vocab_size)
-            expected_probs[tgt[i]]
-            
+            expected_probs = torch.zeros(model.vocab_size).to(device=device)
+            expected_probs[output_tokens[i]] = 1
+    
             loss = criterion(probs, expected_probs)
 
             loss.backward()
             optimizer.step()
 
             total_loss += float(loss)
-            
+
         print("loss:", total_loss / len(output_tokens))
 
 torch.save(model, "trained.pt")
